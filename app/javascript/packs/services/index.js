137282707d04bcb4ad2bb642469c106b61f8e3da
shoynoi <akira.yoshino@luxiar.com>
2020-06-29 09:23:11 +0900

    サービス一覧ページにIndexコンポーネントを定義
    - FlashやServiceListコンポーネントをまとめるIndexコンポーネントを定義した
    - 不要なタグが出力されないようにフラグメントで上記コンポーネントを囲うようにした

--- app/javascript/packs/services/index.js
+++ app/javascript/packs/services/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
+import ServiceList from '../../components/ServiceList';
 import Flash from '../../components/Flash';
+
+const Index = () => (
+  <>
+    <Flash />
+    <ServiceList />
+  </>
+);
 
 document.addEventListener('DOMContentLoaded', () => {
   render(
-    <Flash />,
+    <Index />,
     document.querySelector('#root'),
   );
 });
