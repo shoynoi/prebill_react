4fb9208e267a2df15f579fe40f749621fc54ecc6
shoynoi <akira.yoshino@luxiar.com>
2020-06-29 15:10:47 +0900

    ServicesコンポーネントをServiceListコンポーネントにリネーム

--- app/javascript/components/ServiceList.js
+++ app/javascript/components/ServiceList.js
@@ -1,60 +1,16 @@
 import React from 'react';
-import Services from './Services';
+import ServiceItem from './ServiceItem';
 
-class ServiceList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      services: null,
-    };
+const ServiceList = ({ services }) => {
+  if (services.length === 0) {
+    return null;
   }
 
-  componentDidMount() {
-    fetch('/services.json', {
-      method: 'GET',
-      credentials: 'same-origin',
-    })
-      .then((response) => {
-        response.json()
-          .then((services) => {
-            this.setState({ services });
-          });
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }
-
-  render() {
-    const { services } = this.state;
-
-    if (services === null) {
-      return null;
-    }
-
-    return (
-      <div className="page-body">
-        <h3 className="page-body__header">利用中のサービス</h3>
-        <div className="page-body__actions">
-          <a href="/services/new">新規登録</a>
-        </div>
-        <div className="page-body__inner">
-          <div className="list-group">
-            <div className="list-group__labels">
-              <div className="list-group__label col-lg">サービス名</div>
-              <div className="list-group__label col-sm">プラン</div>
-              <div className="list-group__label col-sm">料金</div>
-              <div className="list-group__label">更新日</div>
-              <div className="list-group__label">通知日</div>
-            </div>
-            <div className="list-group__inner">
-              <Services services={services} />
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+  return (
+    services.map((service) => (
+      <ServiceItem service={service} key={service.id} />
+    ))
+  );
+};
 
 export default ServiceList;
