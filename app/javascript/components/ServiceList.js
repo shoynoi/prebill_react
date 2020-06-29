a12e2760fd434033ceacfad9bbc3f8d0f910b569
shoynoi <akira.yoshino@luxiar.com>
2020-06-29 09:29:05 +0900

    ServiceListコンポーネントに要素をレンダリングする処理を追加
    - サービス一覧のヘッダーを表示
    - 個々のサービスのレンダリングはServicesコンポーネントを作成して表示させるようにコンポーネントを分割

--- app/javascript/components/ServiceList.js
+++ app/javascript/components/ServiceList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Services from './Services';
 
 class ServiceList extends React.Component {
   constructor(props) {
@@ -22,6 +23,37 @@ class ServiceList extends React.Component {
       .catch((error) => {
         console.error(error);
       });
+  }
+
+  render() {
+    const { services } = this.state;
+
+    if (services === null) {
+      return null;
+    }
+
+    return (
+      <div className="page-body">
+        <h3 className="page-body__header">利用中のサービス</h3>
+        <div className="page-body__actions">
+          <a href="/services/new">新規登録</a>
+        </div>
+        <div className="page-body__inner">
+          <div className="list-group">
+            <div className="list-group__labels">
+              <div className="list-group__label col-lg">サービス名</div>
+              <div className="list-group__label col-sm">プラン</div>
+              <div className="list-group__label col-sm">料金</div>
+              <div className="list-group__label">更新日</div>
+              <div className="list-group__label">通知日</div>
+            </div>
+            <div className="list-group__inner">
+              <Services services={services} />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
