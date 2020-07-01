b43cd42e8a0475f29effec3eeed56e886501a2a9
shoynoi <akira.yoshino@luxiar.com>
2020-07-01 16:34:57 +0900

    LinkButtonコンポーネントが受け取るpropsに型チェックを定義

--- app/javascript/components/LinkButton.js
+++ app/javascript/components/LinkButton.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const LinkButton = ({ href, children }) => (
   <a href={href}>{children}</a>
 );
 
 export default LinkButton;
+
+LinkButton.propTypes = {
+  href: PropTypes.string.isRequired,
+  children: PropTypes.string.isRequired,
+};
