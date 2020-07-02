e454132a22cab0ab0c8a804f24f039d800248c7e
shoynoi <akira.yoshino@luxiar.com>
2020-07-02 11:55:48 +0900

    edit.jsにEditコンポーネントを作成

--- app/javascript/packs/services/edit.js
+++ app/javascript/packs/services/edit.js
@@ -2,11 +2,19 @@ import React from 'react';
 import { render } from 'react-dom';
 import EditService from '../../components/EditService';
 
+const Edit = ({ serviceId }) => (
+  <div className="container">
+    <main className="page">
+      <EditService serviceId={serviceId} />
+    </main>
+  </div>
+);
+
 document.addEventListener('DOMContentLoaded', () => {
   const el = document.querySelector('#root');
   const { serviceId } = el.dataset;
   render(
-    <EditService serviceId={serviceId} />,
+    <Edit serviceId={serviceId} />,
     el,
   );
 });
