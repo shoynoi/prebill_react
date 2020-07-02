e77f0d37e5f7a6ab381ffeb761e60ce330de9e23
shoynoi <akira.yoshino@luxiar.com>
2020-07-02 11:56:26 +0900

    new.jsにNewコンポーネントを定義

--- app/javascript/packs/services/new.js
+++ app/javascript/packs/services/new.js
@@ -2,9 +2,17 @@ import React from 'react';
 import { render } from 'react-dom';
 import NewService from '../../components/NewService';
 
+const New = () => (
+  <div className="container">
+    <main className="page">
+      <NewService />
+    </main>
+  </div>
+);
+
 document.addEventListener('DOMContentLoaded', () => {
   render(
-    <NewService />,
+    <New />,
     document.querySelector('#root'),
   );
 });
