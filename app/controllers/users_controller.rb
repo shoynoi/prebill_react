3c0ef8ba2adf1ce985cdae3761fe0c3d87aa7a74
shoynoi <akira.yoshino@luxiar.com>
2020-07-06 09:12:52 +0900

    ユーザー登録処理を作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,24 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash.notice = "アカウントを作成しました。"
+      head :ok
+    else
+      respond_to do |format|
+        format.json { render json: @user.errors.full_messages, status: 422 }
+      end
+    end
   end
+
+  private
+    def user_params
+      params.require(:user).permit(
+        :name,
+        :email,
+        :password,
+        :password_confirmation
+      )
+    end
 end
