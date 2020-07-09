6981faf3dcf74537ac2f980cf7bd81f503c7f1bd
shoynoi <akira.yoshino@luxiar.com>
2020-07-09 12:06:12 +0900

    Welcomeコンポーネント内のFlashコンポーネント呼び出しを削除
    - ログイン画面において、Flashコンポーネントを2重に表示してしまっているため。
      - メッセージの表示に問題はなく、1つのFlashメッセージが表示されているが、無駄な呼び出しを排除した
    - Welcomeコンポーネントを利用する各コンポーネント側でFlashコンポーネントを呼び出すようにした

--- app/javascript/packs/application/welcome.js
+++ app/javascript/packs/application/welcome.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Footer from '../../components/Footer';
-import Flash from '../../components/Flash';
 import Header from '../../components/Header';
 
 const Welcome = ({ children }) => (
   <>
     <Header />
     <div className="container">
-      <Flash />
       {children}
     </div>
     <Footer />
--- app/javascript/packs/welcome/index.js
+++ app/javascript/packs/welcome/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Welcome from '../application/welcome';
+import Flash from '../../components/Flash';
 
 const Index = () => (
   <Welcome>
+    <Flash />
     <div>Home#index</div>
   </Welcome>
 );
