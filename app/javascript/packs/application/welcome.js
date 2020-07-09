7906e94fd13e617f1f0f026d44082078ac73b249
shoynoi <akira.yoshino@luxiar.com>
2020-07-09 09:10:19 +0900

    WelcomeコンポーネントにヘッダーとFlashメッセージが表示されるようにコンポーネントを追加

--- app/javascript/packs/application/welcome.js
+++ app/javascript/packs/application/welcome.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Footer from '../../components/Footer';
+import Flash from '../../components/Flash';
+import Header from '../../components/Header';
 
 const Welcome = ({ children }) => (
   <>
+    <Header />
     <div className="container">
+      <Flash />
       {children}
     </div>
     <Footer />
