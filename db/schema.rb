6009615cc234ce7dc71429b4422fb444a3cd1d04
shoynoi <akira.yoshino@luxiar.com>
2020-07-10 14:25:51 +0900

    servicesのuser_idにNOT NULL制約を定義し直すmigrationを作成
    - タスクを実行後にmigrationを実行することで既存のサービスがNOT NULL制約によってエラーにならないようにした

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_09_061450) do
+ActiveRecord::Schema.define(version: 2020_07_10_051127) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
