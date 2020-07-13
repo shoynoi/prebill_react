8b6a347fd14dd7ab9c46d043dcd29ce085203526
shoynoi <akira.yoshino@luxiar.com>
2020-07-13 10:40:47 +0900

    ユーザー情報を更新するAPIを作成
    - ユーザー情報変更ページへのルーティングとアクションを追加
    - ユーザー情報を更新する処理を実装

--- app/controllers/my_account_controller.rb
+++ app/controllers/my_account_controller.rb
@@ -2,6 +2,9 @@
 
 class MyAccountController < ApplicationController
   skip_before_action :require_login
+  before_action :set_user, only: %i(update)
+
+  wrap_parameters :user, include: [:name, :email, :password, :password_confirmation]
 
   def show
     if logged_in?
@@ -12,4 +15,32 @@ class MyAccountController < ApplicationController
       head :no_content
     end
   end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash.notice = "ユーザー情報を変更しました。"
+      head :ok
+    else
+      respond_to do |format|
+        format.json { render json: @user.errors.full_messages, status: 422 }
+      end
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(
+        :name,
+        :email,
+        :password,
+        :password_confirmation
+      )
+    end
+
+    def set_user
+      @user = current_user
+    end
 end
