9ecdabe23da3dba3c32d53e98286b2a02cc45a3b
shoynoi <akira.yoshino@luxiar.com>
2020-07-13 10:59:20 +0900

    ユーザーのバリデーションを修正
    - passwordを入力して、password_confirmationが未入力の時に処理が成功していた
      - ref: https://railsguides.jp/active_record_validations.html#confirmation
    - 反対に、password_confirmationを入力して、passwordが未入力のときにも処理が成功していた(パスワードは変更されていないが、修正したというメッセージとともにトップページにリダイレクトされていた)
      - password_required?がfalseを返し、検証されないバリデーションがあった

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,12 @@ class User < ApplicationRecord
   has_many :services, dependent: :destroy
 
   validates :password, length: { minimum: 6 }, confirmation: true, if: :password_required?
+  validates :password_confirmation, presence: true, if: :password_required?
   validates :email, uniqueness: true, presence: true
   validates :name, presence: true
 
   private
     def password_required?
-      new_record? || password.present?
+      new_record? || password.present? || password_confirmation.present?
     end
 end
