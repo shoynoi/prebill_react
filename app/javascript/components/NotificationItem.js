51ec571bdfe7923c7425f28b59348382553f0f76
shoynoi <akira.yoshino@luxiar.com>
2020-07-16 15:12:54 +0900

    NotificationItemコンポーネントを作成
    - 通知をクリックした時に通知が既読となるようにした
    - 未読の通知にマークが表示できるようにするため、既読状態に応じてスタイルを切り替えるようにした
    - 現状、li要素にクリックイベントを付与しているが、ESLintに警告を受ける。
      - jsx-a11y/no-noninteractive-element-interactions
      - jsx-a11y/click-events-have-key-events

---
+++ app/javascript/components/NotificationItem.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const NotificationItem = ({ notification, onClick }) => (
+  <li className="header-dropdown__item" onClick={() => onClick(notification)}>
+    <div className="header-dropdown__container">
+      <div className={`header-dropdown__unread-icon ${notification.read ? 'is-read' : ''}`} />
+      <div className="header-dropdown__text-holder">
+        <p className="header-dropdown__text">
+          {notification.message}
+        </p>
+        <time className="header-dropdown__time-holder">
+          {`${notification.created_at}前`}
+        </time>
+      </div>
+    </div>
+  </li>
+);
+
+export default NotificationItem;
+
+NotificationItem.propTypes = {
+  notification: PropTypes.shape({
+    message: PropTypes.string,
+    created_at: PropTypes.string,
+    read: PropTypes.bool,
+  }).isRequired,
+  onClick: PropTypes.func.isRequired,
+};
