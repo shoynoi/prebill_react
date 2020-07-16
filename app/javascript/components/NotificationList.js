3d84958bcb42a69893027f5985eec7cbd028ae89
shoynoi <akira.yoshino@luxiar.com>
2020-07-16 14:32:20 +0900

    通知一覧を表示するNotificationListコンポーネントを定義
    - 通知一覧の外側をクリックした時に自動的に閉じられるようにonClickOutsideライブラリを利用した
    - 個々の通知についてはNotificationItemコンポーネントを作成した
      - 通知をクリックした時に通知を既読状態にできるようにするために、onClick()をpropsとして渡している

---
+++ app/javascript/components/NotificationList.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import onClickOutside from 'react-onclickoutside';
+import NotificationItem from './NotificationItem';
+
+const NotificationList = ({ onClick, closeDropdown, notifications }) => {
+  NotificationList.handleClickOutside = () => closeDropdown();
+
+  return (
+    <div className="header-dropdown is-notification">
+      <div className="header-dropdown__header">
+        通知
+      </div>
+      <ul className="header-dropdown__items">
+        { notifications.map((notification) => (
+          <NotificationItem
+            notification={notification}
+            onClick={onClick}
+            key={notification.id}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const clickOutsideConfig = {
+  handleClickOutside: () => NotificationList.handleClickOutside,
+};
+
+export default onClickOutside(NotificationList, clickOutsideConfig);
+
+NotificationList.propTypes = {
+  closeDropdown: PropTypes.func.isRequired,
+  onClick: PropTypes.func.isRequired,
+  notifications: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
