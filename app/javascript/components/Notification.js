7331a08d4dc35f84fbfbca5a1b8ccf12677c6746
shoynoi <akira.yoshino@luxiar.com>
2020-07-16 13:54:02 +0900

    通知一覧を表示するコンポーネントをまとめたNotificationコンポーネントを作成
    - 既読状態にするためのmarkAsReact()を定義し、NotificationListコンポーネントに渡すようにした
      - 個々の通知がクリックされた時に発火するように、NotificationListからNotificationItemへと渡すようにしている
      - 未読状態の通知がクリックされたときのみリクエストするようにガード節を定義
      - 既読状態になった時に、未読マークが外れるようにするため、リクエストの成功時に、該当するstateも既読状態となるようにした
    - 未読があることがページ上からわかるようにするために、未読通知がある際に表示されるマークを動的に切り替えるためのhasUnReadを定義した。
    - NotificationListはリスト外をクリックした時に非表示になるようにeventTypesを渡している。
      - defaultでは'click'ではなく'mousedown'だが、'mousedown'だとアイコンをクリックした時に再度通知一覧が開いてしまうため、'click'を指定

---
+++ app/javascript/components/Notification.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import NotificationIcon from './NotificationIcon';
+import NotificationList from './NotificationList';
+import getCsrfToken from '../helpers/getCsrfToken';
+
+class Notification extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notifications: null,
+    };
+    this.toggleDropdown = this.toggleDropdown.bind(this);
+    this.markAsRead = this.markAsRead.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('/api/notifications', {
+      method: 'GET',
+      credentials: 'same-origin',
+    })
+      .then((response) => {
+        response.json()
+          .then((notifications) => {
+            this.setState({ notifications });
+          });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  toggleDropdown() {
+    const { isOpen } = this.state;
+    this.setState({ isOpen: !isOpen });
+  }
+
+  markAsRead(notification) {
+    if (notification.read) return;
+    fetch(`/api/notifications/${notification.id}`, {
+      method: 'PATCH',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+        'X-CSRF-Token': getCsrfToken(),
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          const { notifications } = this.state;
+          const updatedNotifications = notifications.map((prevNotification) => (
+            prevNotification.id === notification.id
+              ? { ...notification, read: true }
+              : prevNotification
+          ));
+          this.setState({
+            notifications: [
+              ...updatedNotifications,
+            ],
+          });
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  render() {
+    const { notifications, isOpen } = this.state;
+
+    if (notifications === null) return null;
+
+    const hasUnRead = notifications.some((notification) => !notification.read);
+
+    return (
+      <div className="header-notification">
+        <NotificationIcon onClick={this.toggleDropdown} hasUnRead={hasUnRead} />
+        { isOpen
+        && (
+          <NotificationList
+            notifications={notifications}
+            eventTypes={['click', 'touchstart']}
+            closeDropdown={() => this.setState({ isOpen: false })}
+            onClick={this.markAsRead}
+          />
+        )}
+      </div>
+    );
+  }
+}
+
+export default Notification;
--- app/javascript/components/UserNav.js
+++ app/javascript/components/UserNav.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import HeaderMenu from './HeaderMenu';
+import Notification from './Notification';
 
 const UserNav = ({ user }) => (
   <div className="header-nav__items">
+    <div className="header-nav__item">
+      <Notification />
+    </div>
     <div className="header-nav__item">
       <HeaderMenu user={user} />
     </div>
