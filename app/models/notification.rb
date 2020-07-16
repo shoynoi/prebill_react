d716876b0e1a7490f814fdd0dab762751438f967
shoynoi <akira.yoshino@luxiar.com>
2020-07-16 13:37:29 +0900

    Rakeタスクから通知を作成できるようにするため、Notificationモデルに通知を作成するメソッドを定義
    - デフォルトで未読にしておき、通知欄からクリックすると既読状態にできるようにする予定
    - 通知欄に表示する際に、句点で改行する方が見やすいのではないかと考え、messageには改行を含ませるようにした

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -4,4 +4,12 @@ class Notification < ApplicationRecord
   belongs_to :service
 
   scope :recent, -> { order(created_at: :desc).limit(10) }
+
+  def self.renew_service(service)
+    Notification.create!(
+      service: service,
+      read: false,
+      message: "「#{service.name}」が更新されました。\n次回の更新日は#{I18n.l service.next_renewed_on(1)}です。"
+    )
+  end
 end
