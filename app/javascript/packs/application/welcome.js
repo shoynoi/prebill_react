3613cb5981759ffac82275b50b9be605c808d505
shoynoi <akira.yoshino@luxiar.com>
2020-07-20 17:06:34 +0900

    ランディングページでページ中央に表示されているアプリの説明部分が横全体に広がるように、welcome.jsからcontainerクラスが指定されたdiv要素を削除
    - WelcomeコンポーネントからレンダリングされているLogin.jsやNewUser.jsのスタイルには影響しなかったので不要だと判断し削除した

--- app/javascript/packs/application/welcome.js
+++ app/javascript/packs/application/welcome.js
@@ -6,9 +6,7 @@ import Header from '../../components/Header';
 const Welcome = ({ children }) => (
   <>
     <Header />
-    <div className="container">
-      {children}
-    </div>
+    {children}
     <Footer />
   </>
 );
